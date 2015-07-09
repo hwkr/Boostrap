f756158b17e7c3f1dfd20fa5a77f46df49c46aa7
Ben Hawker <ben@hawker.cc>
2015-07-09 14:31:29 -0700

    Finished up front page

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,16 @@
 
         // On the home page, move the blog icon inside the header 
         // for better relative/absolute positioning.
-
+        
         //$("#blog-logo").prependTo("#site-head-content");
-
+        
+        
+        //Background Check for main text
+        BackgroundCheck.init({
+          targets: '.auto-color',
+          images: '.home-header'
+        });
+        BackgroundCheck.refresh();
     });
 
 }(jQuery));
