2aa547802c5a39e5cbff4d1b631941c5ad529aba
Ben Hawker <ben@hawker.cc>
2015-12-09 16:21:29 -0800

    Finish filtering and move scripts to separate file

---
+++ assets/js/publications.js
@@ -0,0 +1,46 @@
+var options = {
+    valueNames: ['pub-title', 'pub-desc', 'pub-date']
+};
+
+var pubsList = new List('publications', options);
+
+var sortTitle = $("#sort-title");
+var sortDate = $("#sort-date");
+
+sortTitle.tooltip(
+    {
+        title: sortTitleTooltip,
+        trigger: "hover focus click",
+        container: "body",
+        placement: "bottom"
+    }
+)
+sortDate.tooltip(
+    {
+        title: sortDateTooltip,
+        trigger: "hover focus click",
+        container: "body",
+        placement: "bottom"
+    }
+)
+
+sortTitle.click(function () {
+    var order = this.dataset.order;
+    pubsList.sort('pub-title', {order: order});
+    this.dataset.order = order == "asc" ? "desc" : "asc";
+    sortTitle.html("<i class=\"fa fa-sort-alpha-" + this.dataset.order + "\"></i>");
+})
+
+sortDate.click(function () {
+    var order = this.dataset.order;
+    pubsList.sort('pub-date', {order: order});
+    this.dataset.order = order == "asc" ? "desc" : "asc";
+    sortDate.html("<i class=\"fa fa-sort-numeric-" + this.dataset.order + "\"></i>");
+})
+
+function sortTitleTooltip(){
+    return ("Sort by Title " + (this.dataset.order == "asc" ? "A-Z" : "Z-A"));
+}
+function sortDateTooltip(){
+    return ("Sort by Year " + (this.dataset.order == "asc" ? "Oldest First" : "Newest First"));
+}
