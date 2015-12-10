14d999b94a76e9ba0d26323f38c85011586dd4bc
Ben Hawker <ben@hawker.cc>
2015-12-10 15:25:16 -0800

    Add filter empty error

--- assets/js/publications.js
+++ assets/js/publications.js
@@ -6,6 +6,21 @@ var pubsList = new List('publications', options);
 
 var sortTitle = $("#sort-title");
 var sortDate = $("#sort-date");
+var filterError = $("#filter-error");
+var currentFilter = $("#current-filter");
+var searchBox = $("input.search");
+
+
+pubsList.on("searchComplete",function(){
+    if (pubsList.visibleItems.length == 0){
+        filterError.removeClass("hidden");
+        currentFilter.text(searchBox.val());
+    }
+    else
+    {
+        filterError.addClass("hidden")
+    }
+});
 
 sortTitle.tooltip(
     {
@@ -14,7 +29,7 @@ sortTitle.tooltip(
         container: "body",
         placement: "bottom"
     }
-)
+);
 sortDate.tooltip(
     {
         title: sortDateTooltip,
@@ -22,21 +37,21 @@ sortDate.tooltip(
         container: "body",
         placement: "bottom"
     }
-)
+);
 
 sortTitle.click(function () {
     var order = this.dataset.order;
     pubsList.sort('pub-title', {order: order});
     this.dataset.order = order == "asc" ? "desc" : "asc";
     sortTitle.html("<i class=\"fa fa-sort-alpha-" + this.dataset.order + "\"></i>");
-})
+});
 
 sortDate.click(function () {
     var order = this.dataset.order;
     pubsList.sort('pub-date', {order: order});
     this.dataset.order = order == "asc" ? "desc" : "asc";
     sortDate.html("<i class=\"fa fa-sort-numeric-" + this.dataset.order + "\"></i>");
-})
+});
 
 function sortTitleTooltip(){
     return ("Sort by Title " + (this.dataset.order == "asc" ? "A-Z" : "Z-A"));
