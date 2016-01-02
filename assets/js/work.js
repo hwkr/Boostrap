00bf11d83f4c28595ca1f177c130d6421456feee
Ben Hawker <ben@hawker.cc>
2016-01-02 01:10:51 -0800

    Update work page styling and add helper

---
+++ assets/js/work.js
@@ -0,0 +1,21 @@
+var options = {
+    valueNames: ['card-title', 'card-meta']
+};
+
+var pubsList = new List('work', options);
+
+var filterError = $("#filter-error");
+var currentFilter = $("#current-filter");
+var searchBox = $("input.search");
+
+
+pubsList.on("searchComplete",function(){
+    if (pubsList.visibleItems.length == 0){
+        filterError.removeClass("hidden");
+        currentFilter.text(searchBox.val());
+    }
+    else
+    {
+        filterError.addClass("hidden")
+    }
+});
